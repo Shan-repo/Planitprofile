ef8a378a2f5bc8356dd4c511afb42509a3e5a177
Shanavas <57465591+Shanavas-Repo@users.noreply.github.com>
2020-09-30 13:30:14 +1000

    Latest code

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -4,7 +4,7 @@
 
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import App from './app/App'
 import store from './app/store'
